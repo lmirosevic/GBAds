25806855682fe8d09e628ef3c2f77993c5343784
Luka Mirosevic <luka@goonbee.com>
2013-01-28 12:09:50 +0100

    Initial Commit

---
+++ GBAds/GBAds.m
@@ -0,0 +1,13 @@
+//
+//  GBAds.m
+//  GBAds
+//
+//  Created by Luka Mirosevic on 28/01/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "GBAds.h"
+
+@implementation GBAds
+
+@end
